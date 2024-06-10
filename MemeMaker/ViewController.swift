7aa7bc944050a9791e0622bdf79ca130ee22b077
J Palacio <jaypalacio95@gmail.com>
2024-06-09 20:51:58 -0400

    Initial Commit

---
+++ MemeMaker/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MemeMaker
+//
+//  Created by juan palacio on 6/9/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
